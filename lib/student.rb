84a60929fca708895aa1251e2e68adaddbcc60dc
Vinny Martinez <vinny@vinnymartinez.com>
2016-12-09 17:00:38 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,28 +2,86 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+    new_row = Student.new
+    new_row.id = row[0]
+    new_row.name = row[1]
+    new_row.grade = row[2]
+    new_row
     # create a new Student object given a row from the database
   end
+
+  def self.first_x_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    LIMIT ?
+    SQL
+    DB[:conn].execute(sql, x)
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    LIMIT 1
+    SQL
+    self.new_from_db(DB[:conn].execute(sql).first)
+  end
+
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
+    DB[:conn].execute(sql).map {|stud| self.new_from_db(stud)}
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.all_students_in_grade_x(x)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, x)
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    new_from_db(DB[:conn].execute(sql, name).first)
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
